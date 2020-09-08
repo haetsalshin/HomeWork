66d32c51b714f604275de7ce17f937cfc0890eef
haetsalshin <sinsin0902@daum.net>
2020-09-08 18:58:21 +0900

    0908 볼링 수정(미완성)

--- Java_HomeWork_004/src/com/biz/game/GameServiceImplV1.java
+++ Java_HomeWork_004/src/com/biz/game/GameServiceImplV1.java
@@ -13,8 +13,9 @@ public class GameServiceImplV1 implements GameService {
 
 	@Override
 	public boolean play() {
-		scan = new Scanner(System.in);
-		gameList = new ArrayList<GameVO>();
+		
+		scan = new Scanner(System.in);	
+		gameList = new ArrayList<GameVO>();	
 		GameVO gameVO = new GameVO();
 		rnd = new Random();
 		
@@ -37,19 +38,31 @@ public class GameServiceImplV1 implements GameService {
 		
 		for(int i = 0; i <10 ; i ++) {
 		
-			System.out.printf("%d번째 판 :" , i+1);
+			System.out.printf("▒▒▒▒▒%d번째 판▒▒▒▒▒\n" , i+1);
 			
 			
 			if(i == 0) {
 				int intScore1 = rnd.nextInt((10)+1);
-				gameVO.setRoundSum1(intScore1);
-				int intScore2 = rnd.nextInt((10)+1);
-				gameVO.setRoundSum2(intScore2);
 				System.out.println("첫번째 라운드");
 				System.out.printf("1번째 : %d , ", intScore1);
-				System.out.printf("2번째 : %d \n", intScore2);
+				gameVO.setScore1(intScore1);
 				
-			}if(i !=0){
+				if((10- intScore1) - rnd.nextInt((10)+1) < 0) {
+					continue;
+					
+				} else if(intScore1 == 10){
+					System.out.printf(" \n");
+					
+				}else {
+					int intScore2 = (10 - intScore1) - rnd.nextInt((10)+1);
+					gameVO.setScore2(intScore2);
+					System.out.printf("2번째 : %d \n", intScore2);
+					
+				}
+				
+				
+				
+			}else if(i !=0){
 				int strike = 10;
 				int spare = 10;
 				
@@ -57,36 +70,49 @@ public class GameServiceImplV1 implements GameService {
 				// 그 전판에서 첫번째에서 스트라이크가 떴을 때
 				if(gameList.get(i-1).getScore1() == 10) {
 					
-					int intScore1 = (rnd.nextInt((10)+1)+strike) ;
-					int intScore2 = (rnd.nextInt((10)+1)+strike);
+					int intScore1 = (rnd.nextInt((10)+1)+strike);
+					if((10- intScore1) - rnd.nextInt((10)+1) < 0) {
+						continue;
+					}
+					int intScore2 = (10 - intScore1) - (rnd.nextInt((10)+1)+strike);
 
 					System.out.println("그 전판에서 첫번째 스트라이크가 떴을 떄");
 					System.out.printf("1번째 : %d , ", intScore1);
 					System.out.printf("2번째 : %d /n", intScore2);
 					
-					gameVO.setRoundSum1(intScore1);
-					gameVO.setRoundSum2(intScore2);
+					gameVO.setScore1(intScore1);
+					gameVO.setScore2(intScore2);
 					
-					// 그 전판에서 두번째에서 스퀘어가 떴을 때
+					
+					
+					// 그 전판에서 두번째에서 스페어가 떴을 때
 				} else if(gameList.get(i-1).getScore2() == 10) {
 					int intScore1 = rnd.nextInt((10)+1)+spare;
-					int intScore2 = rnd.nextInt(10)+1;
+					if((10- intScore1) - rnd.nextInt((10)+1) < 0) {
+						continue;
+					}
+					int intScore2 = (10 - intScore1) -( rnd.nextInt(10)+1);
 					System.out.println("그 전판에서 두번째에서 스퀘어가 떴을 때");
 					System.out.printf("1번째 : %d , ", intScore1);
-					gameVO.setRoundSum1(intScore1);
+					gameVO.setScore1(intScore1);
 					System.out.printf("2번째 : %d /n", intScore2);
-					gameVO.setRoundSum2(intScore2);
+					gameVO.setScore2(intScore2);
 					
 				} else {
 					
 					System.out.println("스퀘어도 스트라이크도 안떴을 때");
 					int intScore1 = rnd.nextInt((10)+1);
-					gameVO.setRoundSum1(intScore1);
-					int intScore2 = rnd.nextInt((10)+1);
-					gameVO.setRoundSum2(intScore2);
+					
+					if((10- intScore1) - rnd.nextInt((10)+1) < 0) {
+						continue;
+					}
+					int intScore2 = (10 - intScore1) - rnd.nextInt((10)+1);
+					
+					
 					System.out.printf("1번째 : %d , ", intScore1);
 					System.out.printf("2번째 : %d \n", intScore2);
-					
+					gameVO.setScore1(intScore1);
+					gameVO.setScore2(intScore2);
 				}
 				
 			}
